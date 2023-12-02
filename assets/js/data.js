2d812708c01f6590c5b771cebaefb207ba506faf
Ramon Rasheed <ramonrash2@gmail.com>
2023-12-02 11:22:53 +0100

    Initial Commit

---
+++ assets/js/data.js
@@ -0,0 +1,16 @@
+const projects = [
+  {
+    name: "Study Buddy",
+    category: "Web Development",
+    imgUrl: "./assets/images/project-study-buddy.png",
+    stack: ["HTML", "CSS", "JS", "DJANGO"],
+    projectLink: "https://github.com/razdacoder/study-buddy",
+  },
+  {
+    name: "Airbnb Clone",
+    category: "Web Development",
+    imgUrl: "./assets/images/project-airbnb.png",
+    stack: ["Next JS", "Tailwind", "Mongo DB", "TypeScript"],
+    projectLink: "https://github.com/razdacoder/airbnb-clone",
+  },
+];
